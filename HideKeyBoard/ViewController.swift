b4b1986783a4ae0a78ef56ebd7a4d00c635a9be3
dgwalkerii1231 <70613928+dgwalkerii1231@users.noreply.github.com>
2020-09-27 18:01:12 -0700

    Updated view controller declaration

--- HideKeyBoard/ViewController.swift
+++ HideKeyBoard/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIScrollViewDelegate, UITextFieldDelegate  {
    
     @IBOutlet weak var btnBack: UIButton!
     @IBOutlet weak var ScrollView: UIScrollView!
